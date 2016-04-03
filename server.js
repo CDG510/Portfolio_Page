60ed3b6f040920bd903d809798c67d96874c60aa
Christian Gonzalez <christian.d.gonzalez.92@gmail.com>
2016-04-03 16:38:46 -0700

    adjusted for H

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.static(path.join(__dirname, './client')));
 require('./server/config/mongoose.js');//for mongoose usage
 require('./server/config/routes.js')(app);
 
-app.listen(5000, function(){
-	console.log('party on 5000')
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response){
+ response.send("<h1> Hello world </h1>");
+});
+app.listen(app.get('port'), function(){
+ console.log("listening on port ", app.get('port'));
 });
